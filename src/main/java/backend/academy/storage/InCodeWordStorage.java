786a57fdbe083aea056d7923359f498242b191eb
shcherbanev <shcherbanev1@yandex.ru>
2024-09-20 15:48:23 +0300

    feat: create storage for words

---
+++ src/main/java/backend/academy/storage/InCodeWordStorage.java
@@ -0,0 +1,49 @@
+package backend.academy.storage;
+
+import backend.academy.entity.Word;
+import backend.academy.types.Category;
+import backend.academy.types.Complexity;
+import java.util.ArrayList;
+import java.util.EnumMap;
+
+public class InCodeWordStorage extends WordStorage {
+
+    public InCodeWordStorage() {
+        wordMap = new EnumMap<>(Category.class);
+        init();
+    }
+
+    private void addWord(Word word) {
+        if (!(wordMap.containsKey(word.category()))) {
+            wordMap.put(word.category(), new EnumMap<>(Complexity.class));
+        }
+        if (!wordMap.get(word.category()).containsKey(word.complexity())) {
+            wordMap.get(word.category()).put(word.complexity(), new ArrayList<>());
+        }
+        wordMap.get(word.category()).get(word.complexity()).add(word);
+    }
+
+    @Override
+    protected final void init() {
+        addWord(new Word(Complexity.EASY, Category.ANIMALS, "cat"));
+        addWord(new Word(Complexity.EASY, Category.ANIMALS, "dog"));
+        addWord(new Word(Complexity.MEDIUM, Category.ANIMALS, "elephant"));
+        addWord(new Word(Complexity.HARD, Category.ANIMALS, "chameleon"));
+
+        addWord(new Word(Complexity.EASY, Category.FOOD, "apple"));
+        addWord(new Word(Complexity.MEDIUM, Category.FOOD, "sandwich"));
+        addWord(new Word(Complexity.HARD, Category.FOOD, "croissant"));
+
+        addWord(new Word(Complexity.EASY, Category.TECHNOLOGY, "mouse"));
+        addWord(new Word(Complexity.MEDIUM, Category.TECHNOLOGY, "laptop"));
+        addWord(new Word(Complexity.HARD, Category.TECHNOLOGY, "microprocessor"));
+
+        addWord(new Word(Complexity.EASY, Category.SPORTS, "ball"));
+        addWord(new Word(Complexity.MEDIUM, Category.SPORTS, "basketball"));
+        addWord(new Word(Complexity.HARD, Category.SPORTS, "triathlon"));
+
+        addWord(new Word(Complexity.EASY, Category.TRAVEL, "train"));
+        addWord(new Word(Complexity.MEDIUM, Category.TRAVEL, "passport"));
+        addWord(new Word(Complexity.HARD, Category.TRAVEL, "itinerary"));
+    }
+}
---
+++ src/main/java/backend/academy/storage/WordStorage.java
@@ -0,0 +1,17 @@
+package backend.academy.storage;
+
+import backend.academy.entity.Word;
+import backend.academy.types.Category;
+import backend.academy.types.Complexity;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+
+@Getter
+public abstract class WordStorage {
+
+    protected Map<Category, Map<Complexity, List<Word>>> wordMap;
+
+    protected abstract void init();
+
+}
