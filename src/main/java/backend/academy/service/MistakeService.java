f8a503905f7ca9f7172d08aa7c427226ca2f6ea3
shcherbanev <shcherbanev1@yandex.ru>
2024-09-21 17:54:24 +0300

    feat: do mistake service

---
+++ src/main/java/backend/academy/service/MistakeService.java
@@ -0,0 +1,37 @@
+package backend.academy.service;
+
+import backend.academy.entity.Word;
+import backend.academy.game.GameConfig;
+import lombok.Getter;
+
+@Getter
+public class MistakeService {
+
+    private int mistakeCount;
+    private final int maxMistakes;
+
+    private static final int MAX_MISTAKES_EASY = GameConfig.EASY_MAX_MISTAKES;
+    private static final int MAX_MISTAKES_MEDIUM = GameConfig.MEDIUM_MAX_MISTAKES;
+    private static final int MAX_MISTAKES_HARD = GameConfig.HARD_MAX_MISTAKES;
+
+    public MistakeService(Word word) {
+        if (word == null || word.complexity() == null) {
+            maxMistakes = MAX_MISTAKES_MEDIUM;
+        } else {
+            switch (word.complexity()) {
+                case EASY -> maxMistakes = MAX_MISTAKES_EASY;
+                case HARD -> maxMistakes = MAX_MISTAKES_HARD;
+                default -> maxMistakes = MAX_MISTAKES_MEDIUM;
+            }
+        }
+    }
+
+    public void incrementMistakeCount() {
+        mistakeCount++;
+    }
+
+    protected boolean isGameOver() {
+        return mistakeCount >= maxMistakes;
+    }
+
+}
