c15f215cfade081fbda33ff1f3bc4564820d408e
shcherbanev <shcherbanev1@yandex.ru>
2024-09-21 16:22:14 +0300

    refactor: use annotation UtilityClass

--- src/main/java/backend/academy/game/GameConfig.java
+++ src/main/java/backend/academy/game/GameConfig.java
@@ -1,10 +1,12 @@
 package backend.academy.game;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class GameConfig {
 
     public static final int EASY_MAX_MISTAKES = 3;
     public static final int MEDIUM_MAX_MISTAKES = 5;
     public static final int HARD_MAX_MISTAKES = 8;
 
-    private GameConfig() {}
 }
