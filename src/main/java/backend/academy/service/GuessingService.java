c7faac16a39a323cbcd7d9faab6dd244290ffc87
shcherbanev <shcherbanev1@yandex.ru>
2024-09-21 18:19:20 +0300

    feat: do GuessingService
    service changes mask in session

---
+++ src/main/java/backend/academy/service/GuessingService.java
@@ -0,0 +1,32 @@
+package backend.academy.service;
+
+import backend.academy.entity.Word;
+import backend.academy.game.GameSession;
+
+public class GuessingService {
+
+    private final Word word;
+
+    public GuessingService(GameSession gameSession) {
+        this.word = gameSession.word();
+        initMask(gameSession.mask());
+    }
+
+    private void initMask(char[] mask) {
+        for (int i = 0; i < word.word().length(); i++) {
+            mask[i] = '*';
+        }
+    }
+
+    public boolean guessLetter(char letter, GameSession gameSession) {
+        boolean guessed = false;
+        for (int i = 0; i < word.word().length(); i++) {
+            if (word.word().charAt(i) == letter) {
+                gameSession.mask()[i] = word.word().charAt(i);
+                guessed = true;
+            }
+        }
+        return guessed;
+    }
+
+}
