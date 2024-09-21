bf16e9fb6751635ae18ef77d49be850dba112ca0
shcherbanev <shcherbanev1@yandex.ru>
2024-09-21 18:18:39 +0300

    feat: add info method to Word

--- src/main/java/backend/academy/entity/Word.java
+++ src/main/java/backend/academy/entity/Word.java
@@ -5,4 +5,7 @@ import backend.academy.types.Complexity;
 
 public record Word(Complexity complexity, Category category, String word) {
 
+    public String getInfo() {
+        return "Category: " + category + " Complexity: " + complexity;
+    }
 }
