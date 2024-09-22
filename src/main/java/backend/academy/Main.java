3da2e459f57fdac5f1229b856eecf072c4ad1e55
shcherbanev <shcherbanev1@yandex.ru>
2024-09-22 15:54:27 +0300

    feat: run game in Main

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,12 @@
 package backend.academy;
 
+import backend.academy.game.Game;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        Game game = new Game(System.out, System.in);
+        game.start();
     }
 }
