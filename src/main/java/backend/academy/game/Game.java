d8deb44ff0d127cd586d3013feb5555d89bf351e
shcherbanev <shcherbanev1@yandex.ru>
2024-09-22 19:45:47 +0300

    fix: update Game class for new architecture

--- src/main/java/backend/academy/game/Game.java
+++ src/main/java/backend/academy/game/Game.java
@@ -2,7 +2,7 @@ package backend.academy.game;
 
 import backend.academy.entity.Word;
 import backend.academy.exception.WordNotFoundException;
-import backend.academy.service.GuessingService;
+import backend.academy.service.InteractionService;
 import backend.academy.service.WordService;
 import backend.academy.service.render.ConsoleRender;
 import backend.academy.storage.word.InCodeWordStorage;
@@ -10,88 +10,72 @@ import backend.academy.types.Category;
 import backend.academy.types.Complexity;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
 
 public class Game {
 
     private GameSession gameSession;
-    private final PrintStream printStream;
-    private final Scanner scanner;
-    private GuessingService guessingService;
+    private final InteractionService interactionService;
     private final ConsoleRender consoleRender;
 
     public Game(PrintStream printStream, InputStream inputStream) {
-        this.printStream = printStream;
-        this.scanner = new Scanner(inputStream, StandardCharsets.UTF_8);
+        this.interactionService = new InteractionService(printStream, inputStream);
         this.consoleRender = new ConsoleRender();
     }
 
     public void start() {
         while (!initWord()) {
-            printStream.println("Try again");
+            interactionService.println("Try again");
         }
-        printStream.println(gameSession.word().getInfo());
+        interactionService.println(gameSession.word().getInfo());
+
         gameProcess();
+
         if (gameSession.isWordGuessed()) {
-            printStream.println("You guessed the word! :D");
+            interactionService.println("You guessed the word! :D");
         } else {
-            printStream.println("You didn't guess the word :(");
+            interactionService.println("You didn't guess the word :(");
         }
     }
 
     private boolean initWord() {
-        printStream.println("Choose category");
-        for (Category category : Category.values()) {
-            printStream.println(category.toString());
-        }
-        Category category = Category.getCategory(scanner.nextLine().toUpperCase());
-        if (category == null) {
-            printStream.println("You input invalid category. Random was chosen");
-        }
-
-        printStream.println("Choose complexity");
-        for (Complexity complexity : Complexity.values()) {
-            printStream.println(complexity.toString());
-        }
-        Complexity complexity = Complexity.getComplexity(scanner.nextLine().toUpperCase());
-        if (complexity == null) {
-            printStream.println("You input invalid complexity. Random was chosen");
-        }
-        WordService wordService = new WordService(new InCodeWordStorage());
-        try {
-            Word word = wordService.getWord(category, complexity);
-            this.gameSession = new GameSession(word);
-            this.guessingService = new GuessingService(gameSession);
-            return true;
-        } catch (WordNotFoundException e) {
-            printStream.println(e.getMessage());
-            return false;
-        }
+        Word word;
+        Category category = interactionService.inputCategory();
+        Complexity complexity = interactionService.inputComplexity();
+        word = generateWord(category, complexity);
+        return word != null;
     }
 
     private void gameProcess() {
         while (gameSession.isAlive()) {
-            printStream.println(consoleRender.render(gameSession.mistakeService().mistakeCount(),
+            interactionService.println(consoleRender.render(gameSession.mistakeService().mistakeCount(),
                 gameSession.mistakeService().maxMistakes()));
-            printStream.println(new String(gameSession.mask()));
-            String input;
-            do {
-                printStream.println("Input only one letter");
-                input = scanner.nextLine().toLowerCase();
-            } while (input.length() != 1 || !Character.isLetter(input.charAt(0)));
-            boolean guessResult = guessingService.guessLetter(input.charAt(0), gameSession);
+            interactionService.println(new String(gameSession.mask()));
+
+            char letter = interactionService.letterInput();
+            boolean guessResult = gameSession.guess(letter);
             if (guessResult) {
-                printStream.println("You guessed the letter");
+                interactionService.println("You guessed the letter");
             } else {
-                gameSession.mistakeService().incrementMistakeCount();
-                printStream.println("Incorrect letter");
+                interactionService.println("Incorrect letter");
             }
-            printStream.println("______________________________________");
+            interactionService.println("______________________________________");
         }
-        printStream.println(consoleRender.render(gameSession.mistakeService().mistakeCount(),
+
+        interactionService.println(consoleRender.render(gameSession.mistakeService().mistakeCount(),
             gameSession.mistakeService().maxMistakes()));
-        printStream.println("Word was: " + gameSession.word().word());
+        interactionService.println("Word was: " + gameSession.word().word());
+    }
+
+    private Word generateWord(Category category, Complexity complexity) {
+        WordService wordService = new WordService(new InCodeWordStorage());
+        try {
+            Word word = wordService.getWord(category, complexity);
+            this.gameSession = new GameSession(word);
+            return word;
+        } catch (WordNotFoundException e) {
+            interactionService.println(e.getMessage());
+            return null;
+        }
     }
 
 }
